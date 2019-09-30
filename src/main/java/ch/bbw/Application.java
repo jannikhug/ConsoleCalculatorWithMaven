35b0e013953a0cb8c6b5479aa751973ac70c8a70
Jannik <Jannik.Hug@swisscom.com>
2019-09-30 11:06:48 +0200

    Rechnung commiten

--- src/main/java/ch/bbw/Application.java
+++ src/main/java/ch/bbw/Application.java
@@ -1,4 +1,22 @@
 package ch.bbw;
 
+/**
+ * @author Jannik Hug
+ * @version 0.0.1
+ */
+
 public class Application {
+
+    public static void main(String[]args){
+        Calculator calculator = new Calculator();
+        int v1;
+        int v2;
+
+        System.out.println("Console Calculator");
+        System.out.println("------------------");
+
+        v1 = 10;
+        v2 = 20;
+        System.out.println("Summe: "+ v1 +"+"+ v2 +"=" + calculator.summe(10, 20));
+    }
 }
--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -1,4 +1,13 @@
 package ch.bbw;
 
+/**
+ * @author Jannik Hug
+ * @version 0.0.1
+ */
+
 public class Calculator {
+
+    public int summe(int s1, int s2){
+        return s1+s2;
+    }
 }
